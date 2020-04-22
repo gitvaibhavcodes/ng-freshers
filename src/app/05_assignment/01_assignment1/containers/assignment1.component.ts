4001fe75fee2730b163ffabc76e7ab952210271b
Vaibhav Jain <vaibhav.jain01@nagarro.com>
2020-04-22 15:59:02 +0530

    Assignment Completed

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-assignment1',
   templateUrl: './assignment1.component.html',
-  styleUrls: ['./assignment1.component.css']
+  styleUrls: ['./assignment1.component.css'],
+  
 })
-export class Assignment1Component {
 
-}
+
+export class Assignment1Component {
+ 
+  value: string;
+  OnWrite(item){
+    console.log(item.target.value)
+    this.value=item.target.value;
+    
+  }
+   
+  }
+
